d8d39bb3bf482f7953c778ef22ba8744b0ad1c04
abinett <abinett@kth.se>
2023-02-08 15:57:10 +0100

    sample projects for the unit testing #6

---
+++ simpleprojects/PosCompPosTest/src/main/java/PosCompPosTest.java
@@ -0,0 +1,21 @@
+
+public class PosCompPosTest {
+
+    public static int[] numbFreq(int[] array){
+        int[] freq = new int[array.length];
+        int visited = -1;
+        for(int i = 0; i < array.length; i++){
+            if(freq[i] != visited) {
+                int count = 1;
+                for(int j = i+1; j < array.length; j++){
+                    if(array[i] == array[j]) {
+                        count++;
+                        freq[j] = -1;
+                    }
+                }
+                freq[i] = count;
+            }
+        }
+        return freq;
+    }
+}
