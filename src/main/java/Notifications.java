8781870954dc0e759c295cefb15e8c3ab84465a6
Hasti Mohebali Zadeh <hastimz@kth.se>
2023-02-08 17:32:40 +0100

    fix: fixing small indentation errors and giving credit #4

--- src/main/java/Notifications.java
+++ src/main/java/Notifications.java
@@ -9,8 +9,8 @@ public class Notifications {
     // used in the next function
     public void post(URL url, String postString) throws IOException {
 
-        // from: https://stackoverflow.com/questions/28114663/java-httpurlconnection-setrequestmethod-doesnt-work
-        // info from: https://www.javatpoint.com/java-url-openconnection-method
+        // inspired from: https://stackoverflow.com/questions/28114663/java-httpurlconnection-setrequestmethod-doesnt-work
+
         HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         //connection.setRequestProperty();
@@ -28,13 +28,12 @@ public class Notifications {
     // Send an HTTP POST to GitHub API to update the status for a given commit
     public void post_status( String OwnerName, String RepoName, String SHA, String status, String description , String targetURL) throws IOException{
 
-        // info from here: https://docs.github.com/en/rest/commits/statuses?apiVersion=2022-11-28#create-a-commit-status
         URL url = new URL("https://api.github.com/repos/" + OwnerName + "/" + RepoName + "/statuses/" + SHA);
 
         String postString = "{\n" +
-                " \"state\": \"" + status + "\",\n" +
+                "  \"state\": \"" + status + "\",\n" +
                 (targetURL != null ? "  \"target_url\": \"" + targetURL + "\",\n" : "") +
-                " \"description\": \"" + description + "\",\n" +
+                "  \"description\": \"" + description + "\",\n" +
                 "}";
 
         post(url, postString);
