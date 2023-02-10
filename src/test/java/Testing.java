1d1fe05df2d8104738ff087e73b8e96075e87b26
abinett <abinett@kth.se>
2023-02-10 17:47:57 +0100

    Feat: Added unit tests for compilation function #25

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -2,9 +2,34 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Testing {
     String absolutePath = System.getProperty("user.dir");
+
+    @Test
+    public void posCompTest(){
+        String pathToPos = absolutePath + "/clonedRepo/simpleprojects/PosCompPosTest";
+        try{
+            HashMap<String, String> info = Features.compileRepo(new HashMap<String, String>(), pathToPos);
+            assertTrue(info.get("Status").equals("Success"));
+        }
+        catch(IOException e){
+            System.out.println("IOException in posCompTest");
+        }
+    }
+    @Test
+    public void negCompTest(){
+        String pathToPos = absolutePath + "/clonedRepo/simpleprojects/NegComp";
+        try{
+            HashMap<String, String> info = Features.compileRepo(new HashMap<String, String>(), pathToPos);
+            assertTrue(info.get("Status").equals("Error"));
+        }
+        catch(IOException e){
+            System.out.println("IOException in negCompTest");
+        }
+    }
+
     @Test
     public void posTest(){
         String pathToPos = absolutePath + "/clonedRepo/simpleprojects/PosCompPosTest";
