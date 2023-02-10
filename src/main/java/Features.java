9b84eacebf3f9b85c351d7b648ffa9257070873c
abinett <abinett@kth.se>
2023-02-10 15:28:31 +0100

    Fix: testfunc working correctly #22

--- src/main/java/Features.java
+++ src/main/java/Features.java
@@ -6,6 +6,8 @@ import java.io.File;
 public class Features {
 
     public static boolean testRepo(String repo) throws IOException {
+        //System.out.println("Repo: " + repo);
+        //System.out.println("File(repo): " + new File(repo));
         boolean testFlag = false;
         //Test
         Process testProcess = Runtime.getRuntime().exec("mvn test", null, new File(repo));
@@ -16,7 +18,9 @@ public class Features {
         }
         String line = "";
         BufferedReader reader = new BufferedReader(new InputStreamReader(testProcess.getInputStream()));
+        //System.out.println("Standard output[tests]:");
         while ((line = reader.readLine()) != null) {
+            //System.out.println(line);
             if(line.contains("BUILD SUCCESS")){
                 testFlag = true;
                 break;
@@ -25,7 +29,7 @@ public class Features {
                 break;
             }
         }
-        System.out.println("Test status: " + testFlag);
+        System.out.println("Test status: " + testFlag + ". REPO: " + repo);
         return testFlag;
     }
 }
